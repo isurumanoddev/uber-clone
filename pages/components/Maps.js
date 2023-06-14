c666e87a1f95a65254e3f794cb246d5948c71bb2
isuru <isurumanod99@gmail.com>
2023-06-14 15:30:17 +0530

    Add function mark location base on given location

--- pages/components/Maps.js
+++ pages/components/Maps.js
@@ -4,29 +4,59 @@
 import mapboxgl from "!mapbox-gl"
 import {useEffect} from "react";
 
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXN1cnVtYW5vZCIsImEiOiJjbGlzdW84MWcwaWY2M2VvejNzbGR0NnNuIn0.FNwzFhyS3DnoRTMNEkgeyw'
 
-function Maps() {
+function Maps({pickupCoordinates, dropoffCoordinates}) {
+
+
     useEffect(() => {
 
         const map = new mapboxgl.Map({
             container: "map",
             style: 'mapbox://styles/mapbox/satellite-streets-v12',
-            center: [80.4912, 6.9271],
+            center: [79.4912, 6.9271],
             zoom: 7.5
         });
 
-        addToMap(map)
+
+    }, []);
+
+    useEffect(() => {
+
+        const map = new mapboxgl.Map({
+            container: "map",
+            style: 'mapbox://styles/mapbox/satellite-streets-v12',
+            center: [79.4912, 6.9271],
+            zoom: 7.5
+        });
 
 
+        if (pickupCoordinates) {
 
+            addToMap(map, pickupCoordinates)
 
-    });
-     const addToMap = (map) => {
-            const marker1 = new mapboxgl.Marker()
-                .setLngLat([79.8449, 6.9238]) // Specify the coordinates of the marker
-                .addTo(map);
         }
+        if (dropoffCoordinates) {
+
+            addToMap(map, dropoffCoordinates)
+
+        }
+
+
+    }, [pickupCoordinates, dropoffCoordinates]);
+
+
+    const addToMap = (map, coordinates) => {
+
+        console.log("pickupCoordinates : ", [coordinates[0], coordinates[1]])
+
+
+        const marker1 = new mapboxgl.Marker()
+        .setLngLat([coordinates[0] ? coordinates[0] : 79.4912,coordinates[1] ? coordinates[1] : 6.9271 ]) // Specify the coordinates of the marker
+        .addTo(map);
+
+    }
 
     return (
         <div className={"flex-1"} id={"map"}></div>
--- pages/confirm.js
+++ pages/confirm.js
@@ -1,32 +1,85 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import "mapbox-gl/dist/mapbox-gl.css"
 
 
 import Maps from "@/pages/components/Maps";
-import Link from "next/link";
+
 
 
 function Confirm() {
-    return (
-        <div className={"flex flex-col h-screen"}>
-            <Maps/>
-            <div className="flex flex-col flex-1">
-                <div className="flex flex-col basis-4/5 bg-amber-800 overflow-y-scroll">
-                    <h1 className="p-4 text-3xl">Hello</h1>
-                    <h1 className="p-4 text-3xl">Hello</h1>
-                </div>
-                <div className={"basis-1/5 bg-amber-100 flex justify-center items-center p-3"}>
-                    <button className=" bg-black text-white w-full py-3 rounded transition  ">Confirm UberX
-                    </button>
-                </div>
 
+    const [pickupCoordinates,setPickupCoordinates] = useState([])
+    const [dropoffCoordinates,setDropoffCoordinates] = useState([])
+
+    const getPickup =async () => {
+
+        const pickUp = "colombo"
+
+
+         const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickUp}.json?` + new URLSearchParams({
+             access_token: "pk.eyJ1IjoiaXN1cnVtYW5vZCIsImEiOiJjbGlzdW84MWcwaWY2M2VvejNzbGR0NnNuIn0.FNwzFhyS3DnoRTMNEkgeyw",
+             limit:1,
+         }))
+        const data =await response.json()
+
+
+        setPickupCoordinates(data["features"][0].center)
+    }
+
+    const getDropoff =async () => {
+
+        const dropOff = "kandy"
+
+
+         const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropOff}.json?` + new URLSearchParams({
+             access_token: "pk.eyJ1IjoiaXN1cnVtYW5vZCIsImEiOiJjbGlzdW84MWcwaWY2M2VvejNzbGR0NnNuIn0.FNwzFhyS3DnoRTMNEkgeyw",
+             limit:1,
+         }))
+        const data =await response.json()
+
+
+        setDropoffCoordinates(data["features"][0].center)
+    }
+
+
+    useEffect(() => {
+        getPickup()
+        getDropoff()
+    }, [])
+
+
+
+
+    return (<div className={"flex flex-col h-screen"}>
+        <Maps
+            pickupCoordinates={pickupCoordinates}
+            dropoffCoordinates={dropoffCoordinates}
+
+        />
+        <div className="flex flex-col flex-1">
+            <div className="flex flex-col basis-4/5  overflow-y-scroll">
+
+                <h1 className="p-4 text-3xl">location 1 </h1>
+                {pickupCoordinates.map(value => (
+
+                    <h1 className="p-4 text-3xl">{value}</h1>
+                ))}
+                <h1 className="p-4 text-3xl">location 2 </h1>
+                {dropoffCoordinates.map(value => (
+
+                    <h1 className="p-4 text-3xl">{value}</h1>
+                ))}
+            </div>
+            <div className={"basis-1/5 bg-amber-50 flex justify-center items-center p-3"}>
+                <button className=" bg-black text-white w-full py-3 rounded transition  ">Confirm UberX
+                </button>
             </div>
 
-
         </div>
-    )
-        ;
+
+
+    </div>);
 }
 
 export default Confirm;
